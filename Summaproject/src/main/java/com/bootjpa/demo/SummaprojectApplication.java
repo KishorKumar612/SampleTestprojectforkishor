30faeea3ef82f67406b8cf7234ebff9f5192f9e6
Admin <Admin@LAPTOP-642462Q0>
2021-07-29 14:57:22 +0530

    please accept my commit

---
+++ Summaproject/src/main/java/com/bootjpa/demo/SummaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.bootjpa.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SummaprojectApplication.class, args);
+	}
+
+}
---
+++ Summaproject/src/test/java/com/bootjpa/demo/SummaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootjpa.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
